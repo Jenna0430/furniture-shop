b038c622f247cc34eed4647dbd1fa97ac6a6d455
Jenna0430 <114163480+Jenna0430@users.noreply.github.com>
2023-10-11 23:55:00 +0100

    displaying the contents of review section

--- src/components/card/ReviewCard.js
+++ src/components/card/ReviewCard.js
@@ -1,22 +1,56 @@
 import React from 'react'
+import {BsFillStarFill} from "react-icons/bs"
 
 const ReviewCard = () => {
   const cardStyle = {
     width: "18rem"
   }
+
+  const reviewContent = [
+      {
+        id : 'i1',
+        image: '',
+        fullName: 'Miranda Kenneth',
+        message: 'Some quick example text to build on the card title and make up the bulk of the cards content.',
+      },
+      {
+        id : 'i2',
+        image: '',
+        fullName: 'John Snow',
+        message: 'Some quick example text to build on the card title and make up the bulk of the cards content.',
+      },
+      {
+        id : 'i3',
+        image: '',
+        fullName: 'Kelly Lindsey',
+        message: 'Some quick example text to build on the card title and make up the bulk of the cards content.',
+      },
+  ];
+
+
   return (
+    
     <div className='reviewCard'>
-      <div class="card" style={{cardStyle}}>
+     {reviewContent.map ((review) => (
+    <div class="card" style={{cardStyle}}>
     <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <h6 class="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="card-link">Card link</a>
-    <a href="#" class="card-link">Another link</a>
+    <h5 class="card-title"><BsFillStarFill/>
+    <BsFillStarFill/>
+    <BsFillStarFill/>
+    <BsFillStarFill/>
+    </h5>
+    <h6 class="card-subtitle mb-2 text-body-secondary"></h6>
+    
+      <p class="card-text">{review.message}</p>
+      <a href="#" class="card-link">{review.image}</a>
+      <a href="#" class="card-link">{review.fullName}</a>
+  
     </div>
- </div>
     </div>
-  )
-}
+    ))}
+
+    </div> 
+  );
+};
 
 export default ReviewCard
--- src/components/experience/Experience.js
+++ src/components/experience/Experience.js
@@ -3,26 +3,39 @@ import image from "../../assets/images/mainImage.jpg"
 import {BsArrowLeft} from "react-icons/bs"
 import {BsArrowRight} from "react-icons/bs"
 import ReviewCard from '../card/ReviewCard'
+import {BsStars} from "react-icons/bs"
+import {FaCouch} from "react-icons/fa"
+import "./experience.css"
 
 const Experience = () => {
   return (
     <div className="experience">
-      <div className="image">
+      <h1 className="secondHeading mt-5 mb-3 text-center">
+        Enjoy <FaCouch className='icon'/> fully furnished space <br />
+        <BsStars className='icon'/> in less than week
+      </h1>
+      <div className="experience-card">
+
+      <div className="experience-image img-fluid img-thumbnail">
         <img src={image} alt="" className='img-fluid'/>
       </div>
 
-      <div>
-        <div>
-         <h1 className="secondHeading">
+      <div className='section container'>
+        <div className="row">
+        <div className='section-heading'>
+         <h1 className="secondHeading col-8">
           Our Customers <br />
           love us
             </h1>
         </div>
      
-        <div>
-        <BsArrowLeft/>
-        <BsArrowRight/>
+        <div className='section-icon col-4'>
+        <BsArrowLeft className='section-1-icon'/>
+        <BsArrowRight className='section-1-icon'/>
         </div>
+
+        </div>
+        
       </div>
 
       <div className="container">
@@ -30,16 +43,16 @@ const Experience = () => {
           <div className="col-3">
             <ReviewCard/>
           </div>
-          <div className="col-3">
+          {/* <div className="col-3">
             <ReviewCard/>
           </div>
           <div className="col-3">
             <ReviewCard/>
-          </div>
+          </div> */}
         </div>
       </div>
-
-      
+      </div>
+     
 
     </div>
   )
